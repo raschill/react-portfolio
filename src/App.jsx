57cba445c3c6d322ef349b394da774dbb2a6c5aa
raschill <rebeccaaschilling@gmail.com>
2024-11-14 22:12:17 -0600

    fixed the margin

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import About from './components/About';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import Navbar from './components/Nav';
+//import Navbar from './components/Nav';
 import Resume from './components/Resume';
 
 import './App.css';
